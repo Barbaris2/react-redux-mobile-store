acf4aac4091dbdac153b98b0e4f15e97404e5c22
Stolyarov <t9818184233@gmailcom>
2021-02-03 14:13:02 +0300

    2

--- src/reducers/phones.js
+++ src/reducers/phones.js
@@ -1,5 +1,20 @@
+import * as R from 'ramda'
+
+import { FETCH_PHONES_SUCCESS } from '../actionTypes.js'
+
 const initialState = {}
 
-export default (state = initialState) => {
-  return state
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    case FETCH_PHONES_SUCCESS:
+      console.log(payload)
+      const newValues = R.indexBy(R.prop('id'), payload)
+      console.log(newValues)
+      return R.merge(state, newValues)
+      break
+
+    default:
+      return state
+      break
+  }
 }
