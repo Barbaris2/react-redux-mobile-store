f4ace04d5ad325e889fa301927982b8f8bdfe961
Stolyarov <t9818184233@gmailcom>
2021-02-06 10:53:24 +0300

    3

--- src/reducers/phones.js
+++ src/reducers/phones.js
@@ -7,9 +7,7 @@ const initialState = {}
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case FETCH_PHONES_SUCCESS:
-      console.log(payload)
       const newValues = R.indexBy(R.prop('id'), payload)
-      console.log(newValues)
       return R.merge(state, newValues)
       break
 
